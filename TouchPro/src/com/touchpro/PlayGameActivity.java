72e274e9a4d714fb7ef5cc42779b7eb6f2010e11
Darrall Flowers <dflowers@iastate.edu>
2014-05-26 12:15:52 -0500

    Add new timer when button is pressed and shuts off when fingure lifts off of it

--- TouchPro/src/com/touchpro/PlayGameActivity.java
+++ TouchPro/src/com/touchpro/PlayGameActivity.java
@@ -2,14 +2,40 @@ package com.touchpro;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Handler.Callback;
+import android.os.Message;
 import android.view.Menu;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnTouchListener;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class PlayGameActivity extends Activity {
+	long startTime = 0;
+	
+	 Handler timerHandler = new Handler();
+	    Runnable timerRunnable = new Runnable() {
+
+	        @Override
+	        public void run() {
+	            long millis = System.currentTimeMillis() - startTime;
+	            int seconds = (int) (millis / 1000);
+	            int minutes = seconds / 60;
+	            seconds = seconds % 60;
+	            final TextView counter = (TextView) findViewById(R.id.counter);
+	            counter.setText(String.format("%d:%02d", minutes, seconds));
+
+	            timerHandler.postDelayed(this, 500);
+	        }
+	    };
+
+	
+
+	
+	
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -21,8 +47,10 @@ public class PlayGameActivity extends Activity {
 			@Override
 			public boolean onTouch(View v, MotionEvent event) {
 				if(event.getAction() == MotionEvent.ACTION_DOWN){
+					timerHandler.postDelayed(timerRunnable, 0);
 					Toast.makeText(getApplicationContext(), "Button Pressed", Toast.LENGTH_SHORT).show();
 				} else if(event.getAction() == MotionEvent.ACTION_UP){
+					 timerHandler.removeCallbacks(timerRunnable);
 					Toast.makeText(getApplicationContext(), "Button Released", Toast.LENGTH_SHORT).show();
 				}
 				return false;
